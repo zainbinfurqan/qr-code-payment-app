a30a85535bc132324917ad5d429f0a9ff7049058
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-10-21 13:35:28 +0500

    update

--- src/pages/purchase-QR/index.js
+++ src/pages/purchase-QR/index.js
@@ -1,27 +1,49 @@
 import React, { useState } from 'react';
 import { ScrollView, View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import { CONSTANTS } from '../../config/constants'
-
+import QRCodeScanner from 'react-native-qrcode-scanner';
+// import { RNCamera } from 'react-native-camera';
 function PurchaseWithQR(props) {
 
     const [isPurchase, setIsPurchase] = useState(false)
+    const [openQR, setOpenQR] = useState(false)
 
     async function scanQr() {
-        let data = {}
-        data.product = data.id
-        let resProduct = await fetch('', {
-            ...data
-        });
-        let response = await resProduct.json();
-        if (!response.isSold) {
-            let resPurchase = await fetch('');
-            let response = await resPurchase.json()
-            response.isPurchase && setIsPurchase(true)
+        try {
+            // let data = {}
+            // data.product = data.id
+            let product = 'lksadnfadfqwe34afanfj'
+            let resProduct = await fetch(`https://69be15e6f3a2.ngrok.io/api/payment/isvalid?product=${product}`, {
+                method: 'GET',
+                headers: { authorization: "o2k3rofn34n23u40g" },
+            });
+            // console.log(resProduct)
+            if (resProduct.status == 200) {
+
+            } else {
+                let res = await resProduct.json()
+                console.log(res)
+            }
+            // if (resProduct.status == 200) {
+            //     // let response = await resProduct.json();
+            //     // if (!response.isSold) {
+            //     //     let resPurchase = await fetch('/api/payment/purchase');
+            //     //     if (resPurchase.status == 200) {
+            //     //         let response = await resPurchase.json()
+            //     //         response.isPurchase && setIsPurchase(true)
+            //     //     }
+            //     // }
+            // }
+        } catch (error) {
+            console.log(error)
+
         }
+
     }
 
     return (
         <SafeAreaView style={CONSTANTS.container}>
+            {scanQr()}
             <View style={CONSTANTS.container}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                     <TouchableOpacity
@@ -30,17 +52,17 @@ function PurchaseWithQR(props) {
                         <Image style={{ height: 25, width: 25 }} source={require('../../assets/icons/back.png')} />
                     </TouchableOpacity>
                     <TouchableOpacity
-                        // onPress={() => setOpenRechargePanel(!openRechargePanel)}
+                        onPress={() => setOpenQR(!openQR)}
                         style={{ borderWidth: 0.35, alignSelf: 'flex-end', margin: 5, padding: 5, borderRadius: 5, }}>
                         <Text>Open QR </Text>
                     </TouchableOpacity>
                 </View>
-                <View style={{ flex: 1, justifyContent: 'center', }}>
+                {openQR && <View style={{ flex: 1, justifyContent: 'center', }}>
                     {/* <TouchableOpacity onPress={() => closeQRCodeScanner()}><Text>Close</Text></TouchableOpacity> */}
                     {/* <QRCodeScanner
                     // onRead={onSuccess}
                     /> */}
-                </View>
+                </View>}
                 {/* <TouchableOpacity style={{ borderWidth: 1, margin: 5, padding: 5, borderRadius: 5, alignSelf: 'flex-start' }}>
                     <Text style={{ fontWeight: 'bold' }}>Purchase</Text>
                 </TouchableOpacity> */}
